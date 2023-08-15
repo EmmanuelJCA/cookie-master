0ddd8c3dd15b88f03a2b39248f73d9484944ba0b
EmmanuelJCA <emmanuelcanate@gmail.com>
2023-08-14 23:02:13 -0400

    initial pages and components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import { NextPage } from 'next';
+import { Layout } from '@/components/layouts';
 
 
 const Home: NextPage = () => {
   return (
-    <>
-    <h1>Hola Mundo</h1>
-    </>  
+    <Layout>
+      <h1>Hola Mundo</h1>
+    </Layout>  
   )
 }
 
